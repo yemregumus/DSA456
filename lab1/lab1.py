593685b5058cd06405eeb6af9b5c78496e352a5c
Yunus Emre Gumus <ygumus@myseneca.ca>
2023-09-18 15:02:30 -0400

    done1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -31,7 +31,9 @@ def wins_rock_scissors_paper(player, opponent):
 # that we will call n in this description. function returns n! (pronounced n factorial). 
 # n! = n * (n-1) * (n-2)... * 1 Thus, 3! = 3 * 2 * 1. Note that 0! is 1 by definition.
 def factorial(n):
-    if int(n) and n >= 0:
+    if n==0:
+        return 1
+    else:    
         fact = 1
         for i in range(1 , n+1):
             fact *= i   
