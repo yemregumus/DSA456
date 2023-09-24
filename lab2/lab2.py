51c602ddb498366241f1e8fa751165f4a0cee488
yemregumus <ygumus@myseneca.ca>
2023-09-24 10:57:10 -0400

    lab 2 some work done

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -1,7 +1,24 @@
 # copy over your code from lab 1 to this file
 
-def fibonacci():
+def fibonacci(n):
+    if n < 0:
+        print('Incorrect input')
+    elif n == 0:
+        return 0
+    elif n == 1 or n == 2:
+        return 1
 
-def sum_to_goal():
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+def sum_to_goal(numbers, goal):
+    l = len(numbers)
+    for i in range(l):
+        possibleSecond = goal - numbers[i]
+
+        # Check if second number exists within array
+
+        if possibleSecond in numbers:
+            return numbers[i] * possibleSecond
+    return 0
 
     
