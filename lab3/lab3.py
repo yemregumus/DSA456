ee9d01a584c0533897f338a649c885e5be24194d
yemregumus <ygumus@myseneca.ca>
2023-09-30 16:11:57 -0400

    lab3 code done

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -2,13 +2,14 @@
 # Author: YUNUS EMRE GUMUS
 # Student Number:150331197
 #
-# Place the code for your lab 3 here.  Read the specs carefully.  
-# Function name must be exactly as provided.  
+# Place the code for your lab 3 here.  Read the specs carefully.
+# Function name must be exactly as provided.
 # Names of variables and parameters can be whatever you wish it to be
 #
 # To test, run the following command :
 #     python test_lab3.py
 #
+
 
 def factorial(number):
     if number == 0:
@@ -18,6 +19,7 @@ def factorial(number):
     else:
         return number * factorial(number - 1)
 
+
 def linear_search(values, key):
     if len(values) == 0:
         return -1
@@ -25,21 +27,25 @@ def linear_search(values, key):
         if values[0] == key:
             return 0
         else:
-            return 1 + linear_search(values[1:], key)
+            result = linear_search(values[1:], key)
+            if result == -1:
+                return -1
+            else:
+                return result + 1
 
 
-def binary_search(list, key, low=0, high=None):
-	if not isinstance(high, int):
-		high = len(list)-1
+def binary_search(list, key, tail=0, head=None):
+    if head is None:
+        head = len(list) - 1
 
-	if low > high:
-		return -1
+    if tail > head:
+        return -1
 
-	mid = (low + high) // 2
+    middle = (head + tail) // 2
 
-	if list[mid] == key:
-		return mid
-	elif list[mid] < key:
-		return binary_search(list,key,mid+1,high)
-	else:
-		return binary_search(list,key, mid-1, high)
+    if list[middle] == key:
+        return middle
+    elif list[middle] < key:
+        return binary_search(list, key, middle + 1, head)
+    else:
+        return binary_search(list, key, tail, middle - 1)
